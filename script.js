c52704181bb9f0826c61a03d583372a6f77d7d28
BrandonR5 <113938730+brandonrausch07@users.noreply.github.com>
2023-01-11 18:20:47 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+  <h1>Are there tryouts?</h1>
+  <p>Luckily, there are no tryouts in order to be part of the Track Team! However, this does not mean that all team members will go to meets. Only some of the best will go. </p>
+
+  <h1>How do you earn a Varsity Letter?</h1>
+  <p>A Varsity letter will be awarded to athletes who have shown great achievement. This letters can be awarded in one of two ways. The first way is to go to a championship meet. Such as Countys or Shores. The second way is to get a varsity time in the event you are in. The times will be listed on the next page.</p>
+
+  <h1>When are the meets?</h1>
+  <p>The meet schedule below will cover all of the meets that our school can go to</p>
